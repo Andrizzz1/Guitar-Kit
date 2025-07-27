2a1f3c68bbe9da5b87d01415f86b4fc13e87a8cc
Andrizzz1 <andreimandapat09@gmail.com>
2025-07-28 00:01:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,27 +27,27 @@ document.addEventListener("keydown", (event)=>{
 function playSound(key){
         switch (key) {
         case "C":
-            const C_chord = new Audio("sounds/C_major.wav");
+            const C_chord = new Audio("./sounds/C_major.wav");
             C_chord.play()
             break;
         case "G":
-            const G_chord = new Audio("sounds/G_major.wav");
+            const G_chord = new Audio("./sounds/G_major.wav");
             G_chord.play()
             break;
         case "D":
-            const D_chord = new Audio("sounds/guitar-chord-d-minor_140bpm_D_minor-[AudioTrimmer.com].wav");
+            const D_chord = new Audio("./sounds/guitar-chord-d-minor_140bpm_D_minor-[AudioTrimmer.com].wav");
             D_chord.play()
             break;
         case "Em":
-            const Em_chord = new Audio("sounds/E Minor Chord On Guitar _ Diagram & Sound-[AudioTrimmer.com].mp3");
+            const Em_chord = new Audio("./sounds/E Minor Chord On Guitar _ Diagram & Sound-[AudioTrimmer.com].mp3");
             Em_chord.play();
             break;
         case "Am":
-            const Am_chord = new Audio("sounds/guitar-chord-a-minor_140bpm_A_minor-[AudioTrimmer.com].wav");
+            const Am_chord = new Audio("./sounds/guitar-chord-a-minor_140bpm_A_minor-[AudioTrimmer.com].wav");
             Am_chord.play()
             break;
         case "F":
-            const F_chord = new Audio("sounds/guitar-chord-f-major_F_major-[AudioTrimmer.com].wav");
+            const F_chord = new Audio("./sounds/guitar-chord-f-major_F_major-[AudioTrimmer.com].wav");
             F_chord.play()
             break;
         default:
